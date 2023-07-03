eac9f1584c642063351c6f9322f14e3e96c8b297
atos-tsab <thomas.sablotny@atos.net>
2023-07-03 11:32:17 +0200

    S4-3083: App is obsolete and has been deleted.

--- adhoc_lb_to_hu/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["z/adhoclbtohu/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- adhoc_lb_to_hu/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"z/adhoc_lb_to_hu/test/unit/controller/AdhocLbToHu.controller"
-], function () {
-	"use strict";
-});
