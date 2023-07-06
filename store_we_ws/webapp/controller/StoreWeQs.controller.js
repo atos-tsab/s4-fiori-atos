5859c93207beae71f98fdbcb72f6faf443ecd893
atos-tsab <thomas.sablotny@atos.net>
2023-07-06 12:14:37 +0200

    S4-3080: Bugs S4-10150 and S4-10334 solved.

--- store_we_ws/webapp/controller/StoreWeQs.controller.js
+++ store_we_ws/webapp/controller/StoreWeQs.controller.js
@@ -185,11 +185,15 @@
                     this.sViewMode = oScanData.viewMode;
                     
                     if (sManNumber !== null && sManNumber !== undefined && sManNumber !== "") {
-                        sManNumber = oScanData.valueManuallyNo.trim();
+                        // ---- Check for DMC All parameter
+                        sManNumber = this._handleDMC(this.sViewMode, sManNumber);
+
+                        sManNumber = sManNumber.trim();
                         sManNumber = sManNumber.toUpperCase();
                         sManNumber = this._removePrefix(sManNumber);
                         
                         this.iHU = sManNumber;
+
                         this.oScanModel.setProperty("/valueManuallyNo", sManNumber);
                     } else {
                         sManNumber = "";
@@ -823,23 +827,23 @@
                     this.sViewMode = oScanModel.getData().viewMode;
 
                     if (sManNumber !== null && sManNumber !== undefined && sManNumber !== "") {
-                        sManNumber = oEvent.getParameter("valueManuallyNo").trim()
+                        // ---- Check for Data Matix Code
+                        sManNumber = this._handleDMC(this.sViewMode, sManNumber);
+
+                        sManNumber = sManNumber.trim()
                         sManNumber = sManNumber.toUpperCase();
+                        sManNumber = this._removePrefix(sManNumber);
 
                         oScanModel.setProperty("/valueManuallyNo", sManNumber);
                     }
 
                     if (sScanNumber !== null && sScanNumber !== undefined && sScanNumber !== "") {
-                        sScanNumber = oEvent.getParameter("valueScan").trim()
-                        sScanNumber = sManNumber.toUpperCase();
-                        
                         // ---- Check for Data Matix Code
-                        var check = tools.checkForDataMatrixArray(sScanNumber);
+                        sScanNumber = this._handleDMC(this.sViewMode, sScanNumber);
 
-                        if (check[0]) {
-                            var sScanNumber = check[1];
-                        }
-
+                        sScanNumber = sScanNumber.trim()
+                        sScanNumber = sScanNumber.toUpperCase();
+                        
                         oScanModel.setProperty("/valueManuallyNo", sScanNumber);
                         sManNumber = sScanNumber;
                     }
@@ -1027,55 +1031,57 @@
 
 			// ---- Set the Shortcut to buttons
 			$(document).keydown($.proxy(function (evt) {
-                var controlF2 = that.byId("idInput_HU");
+                // var controlF2 = that.byId("idInput_HU");
 
                 // ---- Now call the actual event/method for the keyboard keypress
-                switch (evt.keyCode) {
-			        case 13: // ---- Enter Key
-                        evt.preventDefault();
+                if (evt.keyCode !== null && evt.keyCode !== undefined) {
+                    switch (evt.keyCode) {
+                        case 13: // ---- Enter Key
+                            evt.preventDefault();
 
-                        if (that.iScanModusAktiv < 2) {
-                            that.onPressOk();
-                        } else {
-                            that.iScanModusAktiv = 0;
-                        }
-
-                        evt.keyCode = null;
-
-                        break;			                
-                    case 112: // ---- F1 Key
-                        evt.preventDefault();
-                        var controlF1 = this.BookButton;
-
-				        if (controlF1 && controlF1.getEnabled()) {
-                            that.onPressBooking();
-                        }
-						
-						break;			                
-                    case 113: // ---- F2 Key
-                        evt.preventDefault();
-
-                        if (that.iScanModusAktiv < 2) {
-                            if (controlF2 && controlF2.getEnabled()) {
-                                controlF2.fireChange();
+                            if (that.iScanModusAktiv < 2) {
+                                that.onPressOk();
+                            } else {
+                                that.iScanModusAktiv = 0;
                             }
-                        }
 
-                        evt.keyCode = null;
-                        
-                        break;			                
-                    case 114: // ---- F3 Key
-                        evt.preventDefault();
-                        that.onNavBack();
-						break;			                
-                    case 115: // ---- F4 Key
-                        evt.preventDefault();
-						that.onPressRefresh();						
-						break;			                
-					default: 
-					    // ---- For other SHORTCUT cases: refer link - https://css-tricks.com/snippets/javascript/javascript-keycodes/   
-                        break;
-				}
+                            evt.keyCode = null;
+
+                            break;			                
+                        case 112: // ---- F1 Key
+                            // evt.preventDefault();
+                            // var controlF1 = this.BookButton;
+
+                            // if (controlF1 && controlF1.getEnabled()) {
+                            //     that.onPressBooking();
+                            // }
+                            
+                            break;			                
+                        case 113: // ---- F2 Key
+                            // evt.preventDefault();
+
+                            // if (that.iScanModusAktiv < 2) {
+                            //     if (controlF2 && controlF2.getEnabled()) {
+                            //         controlF2.fireChange();
+                            //     }
+                            // }
+
+                            // evt.keyCode = null;
+                            
+                            break;			                
+                        case 114: // ---- F3 Key
+                            // evt.preventDefault();
+                            // that.onNavBack();
+                            break;			                
+                        case 115: // ---- F4 Key
+                            // evt.preventDefault();
+                            // that.onPressRefresh();						
+                            break;			                
+                        default: 
+                            // ---- For other SHORTCUT cases: refer link - https://css-tricks.com/snippets/javascript/javascript-keycodes/   
+                            break;
+                    }
+                }
 			}, this));
 		},
 
@@ -1094,7 +1100,33 @@
             }
         },
 
-         _removePrefix: function (key) {
+		_handleDMC: function (sViewMode, sManNumber) {
+            var sDMC = "";
+
+            if (sManNumber !== null && sManNumber !== undefined && sManNumber !== "") {
+                // ---- Check for Data Matix Code
+                var check = tools.checkForDataMatrixArray(sManNumber);
+
+                // ---- Check for DMC All parameter
+                if (check[0] === true) {
+                    if (sViewMode === "Material") {
+                        sDMC = check[1];
+                    } else if (sViewMode === "Quantity") {
+                        sDMC = check[3];                 
+                    } else if (sViewMode === "Handling") {
+                        sDMC = check[5];                 
+                    } else {
+                        sDMC = sManNumber;
+                    }
+                } else {
+                    sDMC = sManNumber;
+                }
+            }
+
+            return sDMC;
+		},
+
+        _removePrefix: function (key) {
             let str = key;
 
             // ---- Check for P=Material Suffix | Q=Quantity Suffix || S=HandlingUnit Suffix
