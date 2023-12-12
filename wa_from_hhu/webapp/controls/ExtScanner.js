7b574d3fe7da8f5202ed3d544ab6578ab106db5b
atos-tsab <thomas.sablotny@atos.net>
2023-12-12 10:41:06 +0100

    S4-10439: Bugs S4-14837 fixed.

--- wa_from_hhu/webapp/controls/ExtScanner.js
+++ wa_from_hhu/webapp/controls/ExtScanner.js
@@ -435,7 +435,9 @@ sap.ui.define([
         },
 
         onScannerOkPress: function () {
-            this._oID.close();
+            if (this._oID) {
+                this._oID.close();
+            }
 
             var check = this._validateOkPressed();
             var that  = this;
@@ -1017,8 +1019,8 @@ sap.ui.define([
         },
     
         _validateInputs: function () {
-            var mNumber     = this._oScanModel.getProperty("/valueManuallyNo");
-            var mScanNumber = this._oScanModel.getProperty("/valueScan");
+            var mNumber     = this._oScanModel.getProperty("/valueManuallyNo").trim();
+            var mScanNumber = this._oScanModel.getProperty("/valueScan").trim();
 
             if (mNumber !== null && mNumber !== undefined && mNumber !== "") {                    
                 this._oScanModel.setProperty("/okButton", true);
@@ -1030,8 +1032,8 @@ sap.ui.define([
         },
     
         _validateOkPressed: function () {
-            var mNumber     = this._oScanModel.getProperty("/valueManuallyNo");
-            var mScanNumber = this._oScanModel.getProperty("/valueScan");
+            var mNumber     = this._oScanModel.getProperty("/valueManuallyNo").trim();
+            var mScanNumber = this._oScanModel.getProperty("/valueScan").trim();
 
             if (mNumber !== null && mNumber !== undefined && mNumber !== "") {
                 return true;
