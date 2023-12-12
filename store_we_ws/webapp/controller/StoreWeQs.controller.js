ff6ebe2f529f10e904234cb4b5f5cb9c11793c3c
atos-tsab <thomas.sablotny@atos.net>
2023-12-12 10:40:15 +0100

    S4-3080: Bug S4-14837 fixed.

--- store_we_ws/webapp/controller/StoreWeQs.controller.js
+++ store_we_ws/webapp/controller/StoreWeQs.controller.js
@@ -181,8 +181,10 @@ sap.ui.define([
 			// ---- Enable the Function key solution
 			// this._setKeyboardShortcuts();
 
-            // ---- Set start constellation
+            // ---- Get the default Page ID
             this._getShellSource();
+
+            // ---- Set start constellation
             this._resetAll();
             this.loadUserData();
 
@@ -260,7 +262,7 @@ sap.ui.define([
             if (oScanModel !== null && oScanModel !== undefined) {
                 if (oScanModel.getData() !== null && oScanModel.getData() !== undefined) {
                     var oScanData  = oScanModel.getData();
-                    var sManNumber = oScanData.valueManuallyNo;
+                    var sManNumber = oScanData.valueManuallyNo.trim();
 
                     this.sViewMode = oScanData.viewMode;
                     
@@ -268,7 +270,6 @@ sap.ui.define([
                         // ---- Check for DMC All parameter
                         sManNumber = this._handleDMC(this.sViewMode, sManNumber);
 
-                        sManNumber = sManNumber.trim();
                         sManNumber = sManNumber.toUpperCase();
                         sManNumber = this._removePrefix(sManNumber);
                         
@@ -521,6 +522,9 @@ sap.ui.define([
 
                         if (rData !== null && rData !== undefined && rData !== "") {
                             that.sWN = rData.ParameterValue;
+
+                            // ---- Get the Page ID
+                            that._getShellSource();
                         }
                     }
                 });
@@ -952,7 +956,7 @@ sap.ui.define([
 
                         this.oScanModel.setProperty("/valueManuallyNo", key);
                     } else {
-                       this.oScanModel.setProperty("/valueManuallyNo", "");
+                        this.oScanModel.setProperty("/valueManuallyNo", "");
                     }
                 }
 
@@ -1034,16 +1038,15 @@ sap.ui.define([
 
             if (oEvent !== null && oEvent !== undefined) {
                 if (oEvent.getParameter("valueManuallyNo") !== null && oEvent.getParameter("valueManuallyNo") !== undefined) {
-                    var sManNumber  = oEvent.getParameter("valueManuallyNo");
-                    var sScanNumber = oEvent.getParameter("valueScan");
-                    
+                    var sScanNumber = oEvent.getParameter("valueScan").trim();
+                    var sManNumber  = oEvent.getParameter("valueManuallyNo").trim();
+
                     this.sViewMode = oScanModel.getData().viewMode;
 
                     if (sManNumber !== null && sManNumber !== undefined && sManNumber !== "") {
                         // ---- Check for Data Matix Code
                         sManNumber = this._handleDMC(this.sViewMode, sManNumber);
 
-                        sManNumber = sManNumber.trim()
                         sManNumber = sManNumber.toUpperCase();
                         sManNumber = this._removePrefix(sManNumber);
 
@@ -1054,7 +1057,6 @@ sap.ui.define([
                         // ---- Check for Data Matix Code
                         sScanNumber = this._handleDMC(this.sViewMode, sScanNumber);
 
-                        sScanNumber = sScanNumber.trim()
                         sScanNumber = sScanNumber.toUpperCase();
                         
                         oScanModel.setProperty("/valueManuallyNo", sScanNumber);
@@ -1196,7 +1198,7 @@ sap.ui.define([
 
 		_getShellSource: function (oEvent) {
 			var spaceID = this.oResourceBundle.getText("SpaceId");
-			var pageID  = this.oResourceBundle.getText("PageId");
+			var pageID  = this._getPageId();
 
             if (spaceID !== null && spaceID !== undefined && spaceID !== "" && pageID !== null && pageID !== undefined && pageID !== "") {
                 this.sShellSource = "#Launchpad-openFLPPage?pageId=" + pageID + "&spaceId=" + spaceID;
@@ -1205,7 +1207,7 @@ sap.ui.define([
                     if (History.getInstance().getPreviousHash() !== null && History.getInstance().getPreviousHash() !== undefined) {
                         var sPreviousHash = History.getInstance().getPreviousHash();
     
-                        if (sPreviousHash.includes("pageId=Z_EEWM_PG_MOBILE_DIALOGS&spaceId=Z_EEWM_SP_MOBILE_DIALOGS")) {
+                        if (sPreviousHash.includes("pageId=" + pageID + "&spaceId=Z_EEWM_SP_MOBILE_DIALOGS")) {
                             this.sShellSource = "#Launchpad-openFLPPage?pageId=" + pageID + "&spaceId=" + spaceID;
 
                             return;
@@ -1216,6 +1218,28 @@ sap.ui.define([
                 this.sShellSource = "#Shell-home";
             }
 		},
+
+        _getPageId: function () {
+			var pageID  = this.oResourceBundle.getText("PageId");
+			var pageID1 = this.oResourceBundle.getText("PageIdWi");
+			var pageID2 = this.oResourceBundle.getText("PageIdHu");
+			var pageID3 = this.oResourceBundle.getText("PageIdBr");
+            var sPageID = pageID;
+
+            if (this.sWN !== null && this.sWN !== undefined && this.sWN !== "") {
+                if (this.sWN === "L001") {
+                    var sPageID = pageID1;
+                } else if (this.sWN === "L007") {
+                    var sPageID = pageID2;
+                } else if (this.sWN === "L009") {
+                    var sPageID = pageID3;
+                } else {
+                    sPageID = pageID;
+                }
+            }
+
+            return sPageID;
+        },
 
 		_getServiceUrl: function () {
             var sService = "";
